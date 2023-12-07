737361728259d47be632911c04b7ab44c2d427a8
mobijalex <mobijalex6@gmail.com>
2023-12-07 01:34:14 -0800

    navbarin progress

---
+++ src/components/Home.jsx
@@ -0,0 +1,44 @@
+import { Box, Container, CssBaseline } from '@mui/material'
+import React from 'react'
+import "./home.css"
+import AppBar from '@mui/material/AppBar';
+
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+const Home = () => {
+  return (
+    <>
+     <CssBaseline />
+      <Container className='mainContainer' maxWidth="xlg" >
+        
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+        
+      </Container>
+    </>
+  )
+}
+
+export default Home
